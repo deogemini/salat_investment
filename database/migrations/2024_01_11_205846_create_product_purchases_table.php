c6ebe46a018fbff81cd5ec7f21985cdece451be0
deogemini <geminideogratias8@gmail.com>
2024-01-12 16:40:22 +0300

    initial setup with commit

---
+++ database/migrations/2024_01_11_205846_create_product_purchases_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_purchases', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->string('price_per_item');
+            $table->string('item_amounts');
+            $table->string('price_total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_purchases');
+    }
+};
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-light" style="background-color: #c1e66d;">
+    <div class="container-fluid">
+      <a class="navbar-brand">Stock Management System</a>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
