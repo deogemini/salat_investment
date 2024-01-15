8ad639c099434d3884290a64b52ae4cc423f0ebb
deogemini <geminideogratias8@gmail.com>
2024-01-15 12:47:42 +0300

    change of font

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard')
 @section('content')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Sales Trends</h1>
+    <h3>Sales Trends</h3>
   </div>
 
   <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
