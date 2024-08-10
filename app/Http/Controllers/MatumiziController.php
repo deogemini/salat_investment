d1feb636a0a271f86e89249d31bfbc36a62680cf
Deo Gemini <geminideogratias8@gmail.com>
2024-08-10 20:49:54 +0300

    update now can be done nice

--- app/Http/Controllers/MatumiziController.php
+++ app/Http/Controllers/MatumiziController.php
@@ -194,14 +194,13 @@ class MatumiziController extends Controller
         return redirect()->route('ainamatumizi.index')->with('success', 'Matumizi Type added successfully');
     }
 
-    public function aina_matumizi_update(Request $request)
+    public function aina_matumizi_update(Request $request, $id)
     {
        // Validate the request
         $request->validate([
             'name' => 'required|string|max:255',
             'description' => 'required|string|max:255',
         ]);
-        $id=$request->id;
 
         // Find the record by ID
         $matumizi = MatumiziType::findOrFail($id);
