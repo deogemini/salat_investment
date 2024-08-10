0a1dfb6446393839bf4af9014206f6f5ef765d2b
Deo Gemini <geminideogratias8@gmail.com>
2024-08-10 21:01:09 +0300

    update withdaw page

--- resources/views/balance_collected/index.blade.php
+++ resources/views/balance_collected/index.blade.php
@@ -121,7 +121,12 @@
                     @endphp
                   </tr>
                   @endforeach
-                  <tr style="border-bottom:2px solid #F0C356;">
+
+              </tbody>
+
+              <tfoot>
+
+                <tr style="border-bottom:2px solid #F0C356;">
                     <th>Grand Total</th>
                     <td></td>
                     <td></td>
@@ -130,7 +135,7 @@
                     <td>{{  formatAmount($grandTotalDeposited) }}</td>
                     <td></td>
                   </tr>
-              </tbody>
+              </tfoot>
 
 
             </table>
