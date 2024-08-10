0ee8cbe8f5167155aa85f868764b641faad3e31e
Deo Gemini <geminideogratias8@gmail.com>
2024-08-10 20:58:41 +0300

    arrange my code well

--- resources/views/invoice/index.blade.php
+++ resources/views/invoice/index.blade.php
@@ -1,62 +1,62 @@
 @extends('layouts.dashboard')
 
 @section('content')
-
-<div class="card">
-    <div class="card-header">
-        <h4 class="card-title mb-0 text-center">Management of Invoices </h4>
-    </div>
-
-    <div class="card-body">
-
-        <div class="row g-4 mb-3 ">
-            <div class="col-sm-auto text-right">
-                <div>
-                    <a href="{{ route('invoices.create') }}" class="btn btn-primary">Create Invoice</a>
-
-                </div>
-            </div>
+    <div class="card">
+        <div class="card-header">
+            <h4 class="card-title mb-0 text-center">Management of Invoices </h4>
         </div>
 
-        <div class="table-responsive table-card mt-3 mb-1">
-            <table class="table table-striped table-sm" id="myDataTable">
-              <thead>
-                <tr>
-                    <th>SN</th>
-        <th>Customer Name</th>
-        <th>Total Amount</th>
-        <th>Status</th>
-        <th>Actions</th>
-    </tr>
-              </thead>
-              <tbody>
-@php
-    $i=1;
-    $grandtotalamount =0;
-@endphp
-                @foreach($invoices as $invoice)
-                <tr>
-                    <td>{{$i++}}</td>
-                    <td>{{ $invoice->customer_name }}</td>
-                    <td>{{ $invoice->total_amount }}</td>
-                    <td>{{ $invoice->is_paid ? 'Paid' : 'Unpaid' }}</td>
-                    <td>
-                        <a href="{{ route('invoices.pdf', $invoice->id) }}" class="btn btn-warning">Download PDF</a>
-                    </td>
-                    @php
-                    $grandtotalamount += $invoice->total_amount;
-                    @endphp
-                </tr>
-                @endforeach
+        <div class="card-body">
 
-                <tr style="border-bottom:2px solid #F0C356;">
-                    <th> Grand Total</th>
-                    <th></th>
-                    <td>{{$grandtotalamount}}</td>
+            <div class="row g-4 mb-3 ">
+                <div class="col-sm-auto text-right">
+                    <div>
+                        <a href="{{ route('invoices.create') }}" class="btn btn-primary">Create Invoice</a>
 
-                </tr>
-              </tbody>
+                    </div>
+                </div>
+            </div>
 
-</table>
-</div>
-@endsection
+            <div class="table-responsive table-card mt-3 mb-1">
+                <table class="table table-striped table-sm" id="myDataTable">
+                    <thead>
+                        <tr>
+                            <th>SN</th>
+                            <th>Customer Name</th>
+                            <th>Total Amount</th>
+                            <th>Status</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php
+                            $i = 1;
+                            $grandtotalamount = 0;
+                        @endphp
+                        @foreach ($invoices as $invoice)
+                            <tr>
+                                <td>{{ $i++ }}</td>
+                                <td>{{ $invoice->customer_name }}</td>
+                                <td>{{ $invoice->total_amount }}</td>
+                                <td>{{ $invoice->is_paid ? 'Paid' : 'Unpaid' }}</td>
+                                <td>
+                                    <a href="{{ route('invoices.pdf', $invoice->id) }}" class="btn btn-warning">Download
+                                        PDF</a>
+                                </td>
+                                @php
+                                    $grandtotalamount += $invoice->total_amount;
+                                @endphp
+                            </tr>
+                        @endforeach
+
+                        <tr style="border-bottom:2px solid #F0C356;">
+                            <th> Grand Total</th>
+                            <th></th>
+                            <td>{{ $grandtotalamount }}</td>
+
+                        </tr>
+                    </tbody>
+
+                </table>
+            </div>
+        @endsection
